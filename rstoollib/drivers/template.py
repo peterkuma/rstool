ffdbb90c4e42defaf844429c63440385c65a820f
Peter Kuma <peter@peterkuma.net>
2020-05-21 11:35:51 +1200

    More complete driver template

--- rstoollib/drivers/template.py
+++ rstoollib/drivers/template.py
@@ -1,23 +1,36 @@
 import ds_format as ds
 
+from rstoollib.headers import HEADER_PTS, HEADER_PROF
+
 # This is a template for a new rstool driver which reads native instrument
 # data and outputs raw- or pts-formatted data.
 #
 # Add the driver to DRIVERS in __init__.py to enable the driver.
 
+HEADER_RAW = {
+	# Raw variables header.
+}
+
 def read(filename):
 	# Read data from filename and return a ds dictionary with raw-formatted
 	# (instrument-dependent) data.
-	return
+	d = {}
+	d['.'] = HEADER_RAW
+	return d
 
 def pts(d):
 	# Convert raw-formatted data in d and return a ds dictionary with
 	# pts-formatted data.
-	return
+	pts = {}
+	pts['.'] = HEADER_PTS
+	return pts
 
 # Optional (alternative to implementing read and pts):
 
 def read_prof(filename):
 	# Read data from filename and return a ds dictionary with prof-formatted
 	# (instrument-dependent) data.
+	prof = {}
+	prof['.'] = HEADER_PROF
+	return prof
 
