d2eab7401310cf600fe0c5e4fb87c03f5a36acd4
Peter Kuma <peter@peterkuma.net>
2024-08-21 14:57:29 +0000

    Rename raw to intermediate (im) format

--- rstool/drivers/template.py
+++ rstool/drivers/template.py
@@ -3,23 +3,23 @@ import ds_format as ds
 from rstool.headers import HEADER_PTS, HEADER_PROF
 
 # This is a template for a new rstool driver which reads native instrument
-# data and outputs raw- or pts-formatted data.
+# data and outputs im- or pts-formatted data.
 #
 # Add the driver to DRIVERS in __init__.py to enable the driver.
 
-HEADER_RAW = {
-	# Raw variables header.
+HEADER_IM = {
+	# Intermadiate variables header.
 }
 
 def read(filename):
-	# Read data from filename and return a ds dictionary with raw-formatted
+	# Read data from filename and return a ds dictionary with im-formatted
 	# (instrument-dependent) data.
 	d = {}
-	d['.'] = HEADER_RAW
+	d['.'] = HEADER_IM
 	return d
 
 def pts(d):
-	# Convert raw-formatted data in d and return a ds dictionary with
+	# Convert im-formatted data in d and return a ds dictionary with
 	# pts-formatted data.
 	pts = {}
 	pts['.'] = HEADER_PTS
