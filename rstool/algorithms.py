272131e3bd5288f68b20413861908a20c1117d37
Peter Kuma <peter@peterkuma.net>
2024-08-21 13:22:04 +0000

    Fix formatting

--- rstool/algorithms.py
+++ rstool/algorithms.py
@@ -196,10 +196,10 @@ def calc_w(*,
 	hur=None, wsat=None, # option 3
 ):
 	'''
-	**calc_w**(\*,\
-	    *p*=`None`, *e*=`None`, # option 1\
-	    *hus*=`None`, # option 2\
-	    *hur*=`None`, *wsat*=`None` # option 3\
+	**calc_w**(\*,\\
+	    *p*=`None`, *e*=`None`, # option 1\\
+	    *hus*=`None`, # option 2\\
+	    *hur*=`None`, *wsat*=`None` # option 3\\
 	)
 
 	Calculate humidity mixing ratio from [option 1] pressure *p* (Pa) and
@@ -249,9 +249,9 @@ def calc_z(*,
 	p1=None, p=None, z=None, # option 2
 ):
 	'''
-	**calc_z**(\*,\
-	    *zg*=`None`, *g*=`None`, # option 1\
-	    *p1*=`None`, *p*=`None`, *z*=`None` # option 2\
+	**calc_z**(\*,\\
+	    *zg*=`None`, *g*=`None`, # option 1\\
+	    *p1*=`None`, *p*=`None`, *z*=`None` # option 2\\
 	)
 
 	Calculate altitude (m) from [option 1] geopotential height *zg* (m) and
@@ -271,9 +271,9 @@ def calc_zg(*,
 	p1=None, p=None, zg=None, # option 2
 ):
 	'''
-	**calc_zg**(\*,\
-	    *z*=`None`, *g*=`None`, # option 1\
-	    *p1*=`None`, *p*=`None`, *zg*=`None` # option 2\
+	**calc_zg**(\*,\\
+	    *z*=`None`, *g*=`None`, # option 1\\
+	    *p1*=`None`, *p*=`None`, *zg*=`None` # option 2\\
 	)
 
 	Calculate geopotential height (m) from [option 1] altitude *z* (m) and
